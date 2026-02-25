263b381f866d73e78a77062d2c72650f99b22710
Alejandro Arevalo <j.arevalog@uniandes.edu.co>
2026-02-25 15:19:52 -0500

    req 5

--- App/logic.py
+++ App/logic.py
@@ -218,7 +218,67 @@ def req_5(catalog):
     """
     Retorna el resultado del requerimiento 5
     """
-    # TODO: Modificar el requerimiento 5
+    inicio = get_time()
+    year = catalog["year"]
+    min_year = min_year if min_year >=2018 else 2018
+    max_year = max_year if max_year <=2025 else 2025
+    
+    mayor_precio = {"price":float("-inf")}
+    min_precio = {"price":float("inf")}
+    
+    total_precio = 0
+    total_tier = 0
+    total_pantalla = 0
+    size = 0
+    
+    for y in range(int(min_year), int(max_year)+1):
+        lista = year[str(y)]
+        nodo = lista["first"]
+        for _ in range(sl.size(lista)):
+            comp = n.get_info(nodo)
+            if comp["resolution"] == resolucion:
+                if (float(comp["price"]) < float(min_precio["price"])) or (float(comp["price"]) == float(min_precio["price"]) and float(comp["weight_kg"]) < float(min_precio["weight_kg"])):
+                    min_precio = comp
+                if (float(comp["price"]) > float(mayor_precio["price"])) or (float(comp["price"]) == float(mayor_precio["price"]) and float(comp["weight_kg"]) < float(mayor_precio["weight_kg"]))  :
+                    mayor_precio = comp
+                
+                total_precio += float(comp["price"])
+                total_tier += int(comp["gpu_tier"])
+                total_pantalla += float(comp["display_size_in"])
+                size += 1
+                
+            nodo = n.get_next(nodo)
+        
+    if size > 0:
+        promedio_precio = round(total_precio/size, 2)
+        promedio_tier = round(total_tier/size)
+        promedio_pantalla = round(total_pantalla/size, 2)
+    else:
+        promedio_precio = 0
+        promedio_tier = 0
+        promedio_pantalla = 0
+        mayor_precio = {"weight_kg": "N/A", "gpu_tier": "N/A", "release_year": "N/A", "resolution": "N/A", "price": "N/A", "display_type": "N/A"}
+        min_precio = {"weight_kg": "N/A", "gpu_tier": "N/A", "release_year": "N/A", "resolution": "N/A", "price": "N/A", "display_type": "N/A"}
+
+    lista_estadisticas = [
+        ["Tiempo de ejecución (ms)", delta_time(inicio, get_time())],
+        ["Filtro Seleccion", "CARO" if boolean else "BARATO"],
+        ["Total computadores", size],
+        ["Precio promedio", promedio_precio],
+        ["Tamaño Pantalla promedio (in)", promedio_pantalla],
+        ["Tier promedio", promedio_tier]
+    ]
+    
+    precio = [
+        ["Precio", mayor_precio["price"] if boolean else min_precio["price"]],
+        ["Tamaño Pantalla", mayor_precio["display_size_in"] if boolean else min_precio["display_size_in"]],
+        ["GPU Tier", mayor_precio["gpu_tier"] if boolean else min_precio["gpu_tier"]],
+        ["Display", mayor_precio["display_type"] if boolean else min_precio["display_type"]],
+        ["Año", mayor_precio["release_year"] if boolean else min_precio["release_year"]],
+        ["Peso", mayor_precio["weight_kg"] if boolean else min_precio["weight_kg"]],
+    ]
+    
+    return lista_estadisticas, precio
     pass
 
 def req_6(catalog):
